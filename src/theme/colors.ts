3f15d2fb6625280d1a559f3b7852b262a092271d
Olli Salo <ullebror@gmail.com>
2024-10-17 13:31:24 +0300

    Update: added common styles and colors are properly implemented

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,28 +1,31 @@
+const colors = {
+    green : {
+        shade100 : '#81C784',
+        shade200 : '#4CAF50',
+    },
 
-export const green = {
-    shade100 : '#81C784',
-    shade200 : '#4CAF50',
-};
+    orange : {
+        shade100 : '#FF9800',
+        shade200 : '#FFB74D',
+    },
 
-export const orange = {
-    shade100 : '#FF9800',
-    shade200 : '#FFB74D',
-};
+    gray : {
+        shade100 : '#F2F2F2',
+        shade200 : '#E0E0E0',
+        shade300 : '#757575',
+        shade400 : '#424242',
+        shade500 : '#616161',
+    },
 
-export const gray = {
-    shade100 : '#F2F2F2',
-    shade200 : '#E0E0E0',
-    shade300 : '#757575',
-    shade400 : '#424242',
-    shade500 : '#616161',
-};
+    red : {
+        shade100 : '#E57373',
+        shade200 : '#FF5252',
+    },
 
-export const red = {
-    shade100 : '#E57373',
-    shade200 : '#FF5252',
-};
-
-export const yellow = {
-    shade100 : '#FFEB3B',
-    shade200 : '#FFF176',
+    yellow : {
+        shade100 : '#FFEB3B',
+        shade200 : '#FFF176',
+    }
 }
+
+export default colors;
---
+++ src/theme/commonStyles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+
+const commonStyles = StyleSheet.create({
+    container: {
+        height: 135,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: colors.green.shade100,
+        borderBottomWidth: 1,
+
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: colors.gray.shade400,
+    }
+
+});
+
+export default commonStyles;
