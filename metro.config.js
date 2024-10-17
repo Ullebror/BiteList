14126d84f04ffdec04145c4f676eb917ad98094c
Olli Salo <ullebror@gmail.com>
2024-10-17 13:32:43 +0300

    Changes to implement typescript properly

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from "expo/metro-config";
+const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push("cjs");
+defaultConfig.resolver.sourceExts.push("cjs", "ts", "tsx");
 
-export default defaultConfig;
+module.exports = defaultConfig;
