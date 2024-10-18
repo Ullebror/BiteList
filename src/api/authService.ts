95e3d034806c4f6deca23429c6155b556dab06d6
Olli Salo <ullebror@gmail.com>
2024-10-18 12:58:18 +0300

    changed auth to authService and added registering, logging in and logging out

---
+++ src/api/authService.ts
@@ -0,0 +1,32 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+
+
+export const registerUser = async (email: string, password: string) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    } catch (error) {
+        console.error('Registration error: ', error);
+        throw error;
+    }
+};
+
+export const loginUser = async (email: string, password: string) => {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    } catch (error) {
+        console.error('Login error: ', error);
+        throw error;
+    }
+};
+
+export const logoutUser = async () => {
+    try {
+        await signOut(auth);
+    } catch (error) {
+        console.error('Logout error: ', error);
+        throw error;
+    }
+}
