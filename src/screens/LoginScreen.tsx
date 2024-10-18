ccd7fc6031d5091315f28702776cea55ce5ea21d
Olli Salo <ullebror@gmail.com>
2024-10-18 12:59:00 +0300

    Created a very basic login logic to LoginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,17 +1,42 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 import TopBar from '../components/TopBar';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { DrawerParamList } from '../types/DrawerParamList';
+import { loginUser } from '../api/authService';
 
 type LoginScreenProps = DrawerScreenProps<DrawerParamList, "Login">;
 
 export default function LoginScreen({ navigation }: LoginScreenProps) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            await loginUser(email, password);
+            navigation.navigate('Home');
+        } catch (error) {
+            setError('Login failed. Please check your credentials.')
+        }
+    }
     
     return (
         <View>
             <TopBar navigation={navigation} screenName="Login" />
-            <Text> Placeholder text </Text>
+            <TextInput
+                placeholder="Email" 
+                value={email}
+                onChangeText={setEmail}
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            {error ? <Text>{error}</Text> : null}
+            <Button title="Login" onPress={handleLogin} />
             
         </View>
     );
