cbff8b3462a7c8d07fa5b84d97ef1d7c5eb261f5
Olli Salo <ullebror@gmail.com>
2024-11-05 11:45:28 +0200

    Started working on HomeScreen search and displaying recipes, preliminary RecipeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,62 @@
-import {  View, Text } from 'react-native';
-import React from 'react';
+import {  
+    View, 
+    TextInput, 
+    Button, 
+    FlatList, 
+    Text, 
+    TouchableOpacity, 
+    Image,
+} from 'react-native';
+import React, { useState } from 'react';
 import TopBar from '../components/TopBar';
 import { HomeScreenProps } from '../types/navigationTypes';
+import { searchRecipes } from '../api/recipeService';
+import commonStyles from '../theme/commonStyles';
 
 export default function HomeScreen({ navigation }: HomeScreenProps) {
+    const [query, setQuery] = useState('');
+    const [recipes, setRecipes] = useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSearch = async () => {
+        setIsLoading(true);
+        const results = await searchRecipes(query);
+        setRecipes(results);
+        setIsLoading(false);
+    };
+
+    const navigateToRecipe = (recipe: any) => {
+        navigation.navigate('Recipe', { recipe });
+    }
+
     return (
         <View>
             <TopBar navigation={navigation} screenName="Home" />
-            <Text> Placeholder text </Text>
+            <View style={commonStyles.searchBar}>
+                <TextInput
+                style={commonStyles.inputs}
+                placeholder="Search for a recipe..."
+                value={query}
+                onChangeText={setQuery}
+                />
+                <Button title="Search" onPress={handleSearch} />
+            </View>
+
+            {/* Loading and Results */}
+            {isLoading ? (
+                <Text style={commonStyles.loadingText}>Loading...</Text>
+            ) : (
+                <FlatList
+                    data={recipes}
+                    keyExtractor={(item) => item.uri}
+                    renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigateToRecipe(item)}>
+                        <Image source={{ uri: item.image }} />
+                        <Text>{item.label}</Text>
+                    </TouchableOpacity>
+                    )}
+                />
+            )}
 
         </View>
 
--- src/screens/RecipeScreen.tsx
+++ src/screens/RecipeScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import TopBar from '../components/TopBar';
 import { RecipeScreenProps } from '../types/navigationTypes';
 
-export default function RecipeScreen({ navigation }: RecipeScreenProps) {
+export default function RecipeScreen({ navigation }: RecipeScreenProps, recipe : any) {
 
     return (
         <View>
