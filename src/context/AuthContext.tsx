13992e657b4dbc92fc7e265354e34f1bd858b8aa
Olli Salo <ullebror@gmail.com>
2024-11-23 13:02:57 +0200

    Improved AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,15 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import
+    React,
+    { createContext, useContext, useState, ReactNode, useEffect }
+from 'react';
+import {onAuthStateChanged, User} from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 // Define the type for the context value
 interface AuthContextType {
+    user: User | null;
     isLoggedIn: boolean;
-    login: () => void;
+    login: (user: User) => void;
     logout: () => void;
 }
 
@@ -13,14 +19,37 @@ export const AuthContext = createContext<AuthContextType | undefined>(undefined)
 // Define the AuthProvider component that will wrap the app
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     // Manage the logged-in state
+    const [user, setUser] = useState<User | null>(null);
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+    useEffect(() => {
+        // Listen for authentication state changes
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+          if (currentUser) {
+            setUser(currentUser);  // Set user if logged in
+            setIsLoggedIn(true);
+          } else {
+            setUser(null);  // Clear user if logged out
+            setIsLoggedIn(false);
+          }
+        });
+    
+        // Cleanup the subscription when component unmounts
+        return () => unsubscribe();
+      }, []);
+
     // Define login and logout functions
-    const login = () => setIsLoggedIn(true);
-    const logout = () => setIsLoggedIn(false);
+    const login = (user: User) => {
+        setUser(user);
+        setIsLoggedIn(true);
+    }
+    const logout = () => {
+        setUser(null);
+        setIsLoggedIn(false);
+    }
 
     return (
-        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+        <AuthContext.Provider value={{ user, isLoggedIn, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
